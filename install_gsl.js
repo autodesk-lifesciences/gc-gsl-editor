d7f259aaa6f6d84dfdc176e3a1fdc809a279442f
Maxwell Bates <max.bates@autodesk.com>
2016-08-15 13:20:41 -0700

    clean up postinstall, so travis should work

--- install_gsl.js
+++ install_gsl.js
@@ -4,10 +4,22 @@ const repoName = 'GSL';
 
 console.log('\n\n\nstarting download + install of GSL');
 
-exec('rm -rf ' + repoName + ' && git clone https://github.com/rupalkhilari/GSL.git && cd '+repoName+' && git checkout json_assembly && ./build.sh', {
-  cwd: __dirname,
-}, function (err, stdout, stderr) {
-  console.log(err);
-  console.log(stdout);
-  console.log(stderr);
-});
+const wrappedExec = (command, directory = __dirname) => {
+	return new Promise((resolve, reject) => {
+		exec(command, {
+			cwd: directory,
+		}, function (err, stdout, stderr) {
+			console.log(err);
+  			console.log(stdout);
+  			console.log(stderr);
+  			resolve();
+		});
+	});
+};
+
+async function installGsl() {
+	await wrappedExec(`rm -rf ${repoName}`);
+	await wrappedExec('git clone https://github.com/rupalkhilari/GSL.git');
+	await wrappedExec('git checkout json_assembly', path.resolve(__dirname, repoName));
+	await wrappedExec('./build.sh', path.resolve(__dirname, repoName));
+};
