aeee31ab24fbf7aea60b4e3482355ced2d687dc7
Rupal Khilari <rupal.khilari@autodesk.com>
2016-09-14 16:55:55 -0700

    [gslEditor] Mono installation script GSL installation script.

---
+++ install-gsl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Clone the repo if GSL directory does not exist
+if [ ! -d 'GSL' ]; then 
+	git clone https://github.com/rupalkhilari/GSL-build.git GSL
+	cd GSL
+# If the existing GSL directory is a git repo then update it
+elif [ -d GSL/.git ]; then
+	cd GSL
+	git pull --verbose
+# Else remove the existing GSL directory and reclone it.
+else
+	rm -rf GSL
+	git clone https://github.com/rupalkhilari/GSL-build.git GSL
+	cd GSL
+fi
+# Checkout the required branch
+git checkout json_assembly_build
