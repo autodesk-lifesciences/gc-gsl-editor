7a4bf15c9037f8649c2062a98ea1c1da8be92a31
Rupal Khilari <rupalkhilari@users.noreply.github.com>
2016-09-27 13:36:46 -0700

    GSL extension - Mono installation (#970)
    * Modified the Dockerfile for Jenkins build.
    
    * [gslEditor] Commenting out installation commands that seem to be preventing some
    mono libraries from getting installed on Docker Ubuntu.

--- tools/install-fsharp.js
+++ tools/install-fsharp.js
@@ -20,9 +20,9 @@ async function installFSharp() {
       commandExists('mono', async function(err, exists) {
         if (err || !exists) {
           await promisedExec('sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y', {}, { forceOutput: true});
-          await promisedExec('sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF', {}, { forceOutput: true});
-          await promisedExec('echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list', {}, { forceOutput: true});
-          await promisedExec('echo "deb http://download.mono-project.com/repo/debian wheezy-libtiff-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list', {}, { forceOutput: true});
+          //await promisedExec('sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF', {}, { forceOutput: true});
+          //await promisedExec('echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list', {}, { forceOutput: true});
+          //await promisedExec('echo "deb http://download.mono-project.com/repo/debian wheezy-libtiff-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list', {}, { forceOutput: true});
           await promisedExec('sudo apt-get update -y', {}, { forceOutput: true});
           await promisedExec('sudo apt-get install mono-devel -yf', {}, { forceOutput: true});
           await promisedExec('sudo apt-get install ca-certificates-mono -yf', {}, { forceOutput: true});
--- tools/install-fsharp.sh
+++ tools/install-fsharp.sh
@@ -16,9 +16,9 @@ if [[ ("$OSTYPE" == "linux-gnu") ||  ("$OSTYPE" == "cygwin") ]]; then
 
     set -x
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-    echo "deb http://download.mono-project.com/repo/debian wheezy-libtiff-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
+    #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    #echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+    #echo "deb http://download.mono-project.com/repo/debian wheezy-libtiff-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
 
     sudo apt-get update -y
     sudo apt-get install mono-devel -yf
