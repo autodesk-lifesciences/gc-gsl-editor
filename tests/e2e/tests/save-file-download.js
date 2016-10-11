ba088a75895d3161679ce98ee439112978f7d98d
Rupal Khilari <rupal.khilari@autodesk.com>
2016-10-11 10:00:41 -0700

    [gslEditor] GSL file download test.

--- tests/e2e/tests/save-file-download.js
+++ tests/e2e/tests/save-file-download.js
@@ -25,12 +25,24 @@ module.exports = {
 
     browser.waitForElementPresent('.GSLEditorLayout', 5000, 'expected extension to render')
 
+    browser
+      .keys([browser.Keys.NULL, browser.Keys.DOWN_ARROW])
+      .pause(50)
+      .keys([browser.Keys.NULL, browser.Keys.DOWN_ARROW])
+      .pause(50)
+      .keys([browser.Keys.NULL, browser.Keys.DOWN_ARROW])
+      .pause(50)
+      .keys([browser.Keys.NULL, browser.Keys.DOWN_ARROW])
+      .pause(50)
+      .keys([browser.Keys.NULL, browser.Keys.DOWN_ARROW])
+      .pause(50)
+
     // Running code to check persistence.
     const codeString = 'pADH4 ; mADH2';
 
     saveCode(browser, codeString);
     
-    downloadItem(browser, 'gsl file');
+    downloadItem(browser, 'Source GSL');
 
     browser
       .pause(2000)
